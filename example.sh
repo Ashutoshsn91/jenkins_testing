7ea4eb5bba953763b30e6037d400afa1dcb3bbce
Ashutosh SN <143912274+Ashutoshsn91@users.noreply.github.com>
2024-02-08 16:33:35 +0530

    Update example.sh

--- example.sh
+++ example.sh
@@ -1,7 +1,8 @@
 if [ "$BUILD_STATUS" = "SUCCESS" ]; then
   echo "Build was successful, running additional steps..."
   # Add additional commands here
-  touch f2
+  touch f3
+  date
 else
   echo "Build failed, skipping additional steps."
 fi
